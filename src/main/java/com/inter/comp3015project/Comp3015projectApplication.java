3e523391cff198daea9803694ecb2443a028dc09
Jose Luis Manners <jose@josemanners.com>
2016-11-06 17:26:09 +0100

    estructura inicial del proyecto

---
+++ src/main/java/com/inter/comp3015project/Comp3015projectApplication.java
@@ -0,0 +1,12 @@
+package com.inter.comp3015project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3015projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp3015projectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/inter/comp3015project/Comp3015projectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inter.comp3015project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Comp3015projectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
